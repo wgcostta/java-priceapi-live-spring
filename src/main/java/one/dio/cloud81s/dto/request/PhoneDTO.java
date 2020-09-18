21fc08c02249669351bffcb16492a7a19c45eabc
Wagner Costa <wg.o.costa@gmail.com>
2020-09-18 04:37:19 -0300

    DTO do Phone

---
+++ src/main/java/one/dio/cloud81s/dto/request/PhoneDTO.java
@@ -0,0 +1,29 @@
+package one.dio.cloud81s.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import one.dio.cloud81s.enums.PhoneType;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PhoneDTO {
+
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private PhoneType type;
+
+    @NotEmpty
+    @Size(min = 13,max = 14)
+    private String number;
+}
