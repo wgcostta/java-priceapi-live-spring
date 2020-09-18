eb3c7ad59b16f7906ad65eaea478629b7340492e
Wagner Costa <wg.o.costa@gmail.com>
2020-09-18 04:37:08 -0300

    DTO do Person

---
+++ src/main/java/one/dio/cloud81s/dto/request/PersonDTO.java
@@ -0,0 +1,41 @@
+package one.dio.cloud81s.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import one.dio.cloud81s.entity.Phone;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PersonDTO {
+
+    private Long Id;
+
+    @NotEmpty
+    @Size(min = 2, max = 100)
+    private String firstName;
+
+    @NotEmpty
+    @Size(min = 2, max = 100)
+    private String lastName;
+
+    @NotEmpty
+    @CPF
+    private String cpf;
+
+    private String birthDate;
+
+    @NotEmpty
+    @Valid
+    private List<Phone> phones;
+}
