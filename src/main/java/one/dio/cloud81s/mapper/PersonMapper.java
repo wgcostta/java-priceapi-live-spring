bd2bc79afd8b4a3e3f9820f53c24e5cc5dba239a
Wagner Costa <wg.o.costa@gmail.com>
2020-09-18 04:35:12 -0300

    Com o Mapper é possível fazer a conversão de objetos, do DTO para a entity

---
+++ src/main/java/one/dio/cloud81s/mapper/PersonMapper.java
@@ -0,0 +1,18 @@
+package one.dio.cloud81s.mapper;
+
+import one.dio.cloud81s.dto.request.PersonDTO;
+import one.dio.cloud81s.entity.Person;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface PersonMapper {
+
+    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
+
+    @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
+    Person toModel(PersonDTO personDTO);
+
+    PersonDTO toDTO(Person person);
+}
