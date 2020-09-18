b9e4f7d00d34dbc58768aced663a74934f1c15d8
Wagner Costa <wg.o.costa@gmail.com>
2020-09-18 04:36:32 -0300

    Ajustado o Package

--- src/main/java/one/dio/cloud81s/service/PersonService.java
+++ src/main/java/one/dio/cloud81s/service/PersonService.java
@@ -1,7 +1,9 @@
-package one.dio.cloud81s;
+package one.dio.cloud81s.service;
 
+import one.dio.cloud81s.dto.request.PersonDTO;
 import one.dio.cloud81s.entity.MessageResponseDTO;
 import one.dio.cloud81s.entity.Person;
+import one.dio.cloud81s.mapper.PersonMapper;
 import one.dio.cloud81s.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,13 +12,17 @@ import org.springframework.stereotype.Service;
 public class PersonService {
     private PersonRepository personRepository;
 
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
+
     @Autowired
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
-    public MessageResponseDTO createPerson(Person person) {
-        Person savedPerson = personRepository.save(person);
+    public MessageResponseDTO createPerson(PersonDTO personDTO) {
+        Person personToSave = personMapper.toModel(personDTO);
+
+        Person savedPerson = personRepository.save(personToSave);
         return MessageResponseDTO
                 .builder()
                 .message("Created person with ID: " + savedPerson.getId())
