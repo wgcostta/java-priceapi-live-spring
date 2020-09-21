88af48ea8b13a614a29e7d7425ca570a62ca372c
Wagner Costa <wg.o.costa@gmail.com>
2020-09-21 06:24:19 -0300

    Autorizando o Cors na aplicação

---
+++ src/main/java/one/dio/cloud81s/security/CorsConfiguration.java
@@ -0,0 +1,12 @@
+package one.dio.cloud81s.security;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*").allowedOrigins("*").allowedHeaders("*");
+    }
+}
