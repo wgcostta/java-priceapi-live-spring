9130c19531742dedf96cc397161d90da0bd542d6
Wagner Costa <wg.o.costa@gmail.com>
2020-09-21 06:38:31 -0300

    no message

--- src/main/java/one/dio/cloud81s/Cloud81sApplication.java
+++ src/main/java/one/dio/cloud81s/Cloud81sApplication.java
@@ -18,7 +18,7 @@ public class Cloud81sApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*");
+				registry.addMapping("/**").allowedMethods("*").allowedOrigins("*").allowedHeaders("*");
 			}
 		};
 	}
